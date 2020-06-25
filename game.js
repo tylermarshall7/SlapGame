eb9634834252946ba9e47a1a3f477354cdafec46
tylermarshall7 <tyler.marshall7@gmail.com>
2020-06-25 15:17:01 -0600

    done, just needs to be pretty

--- game.js
+++ game.js
@@ -1,11 +1,51 @@
 let health = 100
 
+let targetName = "Barney Stinson"
+
+let hits = 0
+
 function slap() {
   health--
+  hits++
+  update()
+}
+
+function punch() {
+  health -= 5
+  hits++
+  update()
+}
+
+function kick() {
+  health -= 10
+  hits++
   update()
 }
 
 function update() {
-  document.getElementById('health').innerText = 
+  document.getElementById('health').innerText = `${health}`
+  document.getElementById('hits').innerText = `${hits}`
+  playagain()
 
+}
+
+
+function playagain() {
+
+  if (health < 0) {
+    
+    reset()
+  }
+
+}
+
+function reset() {
+ let gamereset = confirm ("He's out!  Play again?")
+
+ if (gamereset == true) {
+
+  health = 100
+  hits = 0
+  update()
+ }
 }
